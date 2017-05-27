79507c6f75904f720ccad1b9612d85764fb9291a
tch-datasci <tom.clay.hunt@gmail.com>
2017-05-26 21:54:23 -0700

    Simplify and Header
    Switch out table check for IF NOT EXISTS and add header for info

--- ticker-tracker.py
+++ ticker-tracker.py
@@ -1,21 +1,21 @@
+# ticker-tracker.py
+# tch-datasci
+# 05-26-2017
+#
+# This program is to clean and sort data scraped from
+# stock news and discussion sites. It is meant to create
+# a database of company names, their stock ticker symbols
+# and the exchange their stocks are traded on.
+
 import sqlite3
-import json
-import urllib
 import re
 
-conn = sqlite3.connect('tickerdb.sqlite')
+conn = sqlite3.connect('stockydb.sqlite')
 crsr = conn.cursor()
 
-crsr.execute('''
-DROP TABLE IF EXISTS Companies''')
-
-crsr.execute('''
-
-CREATE TABLE Companies (
+crsr.execute('''CREATE TABLE IF NOT EXISTS Companies (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     name TEXT UNIQUE,
     ticker TEXT UNIQUE,
     exchange TEXT
-);
-
-''')
+)''')
